9fb2c3b5158ef602568ebbecfe2a1cd530d71a17
NoufPentest <noufpentest@gmail.com>
2017-06-23 18:31:44 +0100

    Add files via upload

---
+++ Recursion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method1(1);
+            Method2(55);
+            Console.Read();
+        }
+
+        static void Method1(int i)
+        {
+            if (i == 56) return;
+            Console.WriteLine(i);
+            Method1(i + 1);
+        }
+        static void Method2(int i)
+        {
+            if (i == 0) return;
+            Console.WriteLine(i);
+            Method2(i - 1);
+        }
+    }
+}
